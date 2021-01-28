4ae666b01e762007c20d8357478c46ae4c1da77e
Juan <[jjcsilvaguzman@gmail.com]>
2021-01-28 07:29:32 -0600

    Primer Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
+export const todoList = new TodoList();
 
-const nombre = 'Fernando';
+todoList.todos.forEach( crearTodoHtml );
 
-saludar( nombre );
